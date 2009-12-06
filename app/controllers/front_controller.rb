d156ea52f3b2bc4e3614c5ff219786c7efffeff8
aimee daniells <aimee@mychores.co.uk>
2009-12-06 18:07:42 +0000

    Remove most of the front controller - do not need it. Put up a nice simple front screen.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,27 +2,7 @@ class FrontController < ApplicationController
   skip_before_filter :authenticate
   
   def index
-    render :action => current_site, :layout => current_site
+    render :financial
   end
   
-  def financial; end
-
-  private
-  def current_site
-    current_session_site = session[:current_site]
-    if current_session_site.nil? or !controller_actions.include?(current_session_site)
-      session[:current_site] = random_site
-      return session[:current_site]
-    end
-    
-    current_session_site
-  end
-  
-  def random_site
-    controller_actions.choice
-  end
-  
-  def controller_actions
-    FrontController.public_instance_methods(false).reject { |m| m == 'index' }
-  end
 end
